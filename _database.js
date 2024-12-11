5d640f511b6be4730bfcf8abe6a2de46d945f4cb
kerexaaa <kerexakerexa@gmail.com>
2024-12-11 03:17:51 +0100

    update to new version

---
+++ _database.js
@@ -0,0 +1,38 @@
+import sqlite3 from "sqlite3";
+const DBSOURCE = "./db/db.sqlite";
+const db = new sqlite3.Database(DBSOURCE, (err) => {
+    if (err) {
+        console.error(err.message);
+        throw new Error(`An error occured while connecting to db, line 8 on database.ts: ${err.message}`);
+    }
+    else {
+        console.log(`Connected to sqlite!`);
+        db.run(`CREATE TABLE posts (
+        id integer primary key autoincrement,
+        title text,
+        author text,
+        body text
+      )`, (err) => {
+            if (err) {
+                console.log("Table posts id already created:", err.message);
+            }
+            else {
+                console.log("Table posts is created");
+            }
+        });
+        db.run(`CREATE TABLE comments (
+        id integer primary key autoincrement,
+        author text,
+        comment text,
+        post_id integer unsigned
+      )`, (err) => {
+            if (err) {
+                console.log("Table comments id already created:", err.message);
+            }
+            else {
+                console.log("Table comments is created");
+            }
+        });
+    }
+});
+export default db;
--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import db from "./_database";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -31,6 +32,35 @@ app.get("/new", function (req, res) {
   res.render("new", { activePage: "new" });
 });
 
+app.get("/posts", function (req, res) {
+  const sql = "select * from posts";
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      res.status(400);
+      res.send("database error:" + err.message);
+      return;
+    }
+    res.render("posts", { activePage: "posts", posts: rows });
+  });
+});
+
+app.get("/new_post", function (req, res) {
+  res.render("new_post", { activePage: "new_post" });
+});
+
+app.get("/posts/:id/edit", function (req, res) {
+  const sql = "select * from posts where id = ?";
+  const params = [req.params.id];
+  db.get(sql, params, (err, row) => {
+    if (err) {
+      res.status(400);
+      res.send("database error: " + err.message);
+      return;
+    }
+    res.render("edit_post", { post: row, activePage: "posts" });
+  });
+});
+
 app.post("/contact", function (req, res) {
   res.render("contact_answer", { activePage: "contact", formData: req.body });
 });
@@ -38,6 +68,90 @@ app.post("/contact", function (req, res) {
 app.post("/new", function (req, res) {
   res.render("new_answer", { activePage: "new", formData: req.body });
 });
+
+app.post("/new_post", function (req, res) {
+  const data = [req.body.title, req.body.author, req.body.body];
+  const sql = "INSERT INTO posts (title, author, body) VALUES (?,?,?)";
+  db.run(sql, data, function (err: Error) {
+    if (err) {
+      res.status(400);
+      res.send("database error:" + err.message);
+      return;
+    }
+    res.render("new_post_answer", {
+      activePage: "new_post",
+      formData: req.body,
+    });
+  });
+});
+
+app.post("/posts/:id/edit", function (req, res) {
+  const data = [req.body.title, req.body.author, req.body.body, req.params.id];
+  db.run(
+    `update posts set 
+    title = COALESCE(?,title),
+    author = COALESCE(?,author),
+    body = COALESCE(?,body)
+    where id = ?
+    `,
+    data,
+    function (err: Error) {
+      if (err) {
+        res.status(400), res.send("database error: " + err.message);
+        return;
+      }
+      res.redirect("/posts");
+    }
+  );
+});
+
+app.post("/posts/:id/comment", function (req, res) {
+  const data = [req.body.author, req.body.comment, req.params.id];
+  const sql = "INSERT INTO comments (author, comment, post_id) VALUES (?,?,?)";
+  db.run(sql, data, function(err: Error) {
+    if (err) {
+      res.status(400);
+      res.send("database error:" + err.message);
+      return;
+    }
+    // console.log("comment successfully added to post_id:", req.params.id);
+    res.redirect(`/posts/${req.params.id}`)
+  })
+});
+
+app.get("/posts/:id/delete", function (req, res) {
+  const sql = "DELETE FROM posts WHERE id = ?";
+  const params = [req.params.id];
+  db.get(sql, params, (err, row) => {
+    if (err) {
+      res.status(400);
+      res.send("database error:" + err.message);
+      return;
+    }
+    res.redirect("/posts");
+  });
+});
+
+app.get("/posts/:id", function (req, res) {
+  const postSql = "select * from posts where id = ?";
+  const commentsSql = "select * from comments where post_id = ?";
+  const id = [req.params.id];
+  db.get(postSql, id, (err, post) => {
+    if (err) {
+      res.status(400);
+      res.send("database error: " + err.message);
+      return;
+    }
+    db.all(commentsSql, id, (err, comments) => {
+      if (err) {
+        res.status(400);
+        res.send("db error" + err.message);
+        return;
+      }
+      res.render("show_post", { post: post, comments: comments, activePage: "posts" });
+    });
+  });
+})
 
 app.listen("3000", () => {
   console.log("Server is running on port 3000");
